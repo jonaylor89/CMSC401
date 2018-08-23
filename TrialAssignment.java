6c1cb146b7edc56b037f87e9c5283093fee829c0
John Naylor <jonaylor89@gmail.com>
2018-08-23 12:40:46 -0400

    Initial Commit

---
+++ TrialAssignment.java
@@ -0,0 +1,44 @@
+
+import java.util.Scanner;
+
+public class TrialAssignment {
+
+    static Scanner userInput;
+
+    public static void main(String[] argv) {
+
+        int numOfLines;
+        int firstIndex;
+        int secondIndex;
+        int product;
+        int[] products;
+        String lineStr;
+        String[] line;
+    
+        userInput = new Scanner(System.in);
+
+        numOfLines = userInput.nextInt();
+        userInput.nextLine();
+
+        products = new int[numOfLines];
+        
+        for (int i = 0; i < numOfLines; i++) {
+
+            lineStr = userInput.nextLine();
+            
+            line = lineStr.split(" ");
+
+            firstIndex = Integer.valueOf(line.length) - 1;
+            secondIndex = Integer.valueOf(line.length) - 2;
+
+            products[i] = Integer.valueOf(line[firstIndex]) * Integer.valueOf(line[secondIndex]);
+
+        }
+
+        for (int i = 0; i < numOfLines; i++) {
+            System.out.println(products[i]); 
+        }
+    
+    }
+
+}
