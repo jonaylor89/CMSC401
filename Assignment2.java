0b241e64bb3525eabed38de54b26b91ac7cf6c40
John Naylor <jonaylor89@gmail.com>
2018-10-01 16:08:21 -0400

    Initial Commit

---
+++ Assignment2.java
@@ -0,0 +1,27 @@
+
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class Assignment2 {
+
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] argv) {
+
+        ArrayList<Integer> yCoods = new ArrayList();
+    
+        int numberOfHouses = in.nextInt();
+        in.nextLine();
+
+        for (int i = 0; i < numberOfHoues; i++) {
+            yCoods.add(in.nextInt());
+            in.nextLine();
+        }
+
+
+        // Calculate y cood of powerline
+
+    }
+
+
+}
