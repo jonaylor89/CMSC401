ad74869fa3c097d2d93adf266ead9bff8b40f398
John Naylor <jonaylor89@gmail.com>
2018-11-15 10:36:10 -0500

    Converting from hashmap to class

--- Assignment3.java
+++ Assignment3.java
@@ -1,12 +1,21 @@
 
 import java.util.Scanner;
 import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.stream.IntStream;
 
 class Assignment3 {
 
+    // Scanner to get user input
     static Scanner in = new Scanner(System.in);
 
+    // Temperary data structure to store nodes 
+    static HashMap<Integer, Node> nodes = new HashMap<Integer, Node>();
+
     public static void main(String[] argv) {
+
+        Graph g = new Graph();
 
         int numOfCities = in.nextInt();
         in.nextLine();
@@ -14,29 +23,100 @@ class Assignment3 {
         int numOfHighways = in.nextInt();
         in.nextLine();
 
-        HashMap<Integer, Integer> motelPrices = new HashMap<Integer, Integer>();
-        for (int i = 0; i <= motelPrice.length; i++) {
+        for (int i = 0; i <= numOfCities; i++) {
             int cityNumber = in.nextInt();
 
             int motelPrice = in.nextInt();
-            int.nextLine();
+            in.nextLine();
 
-            motelPrices.put(cityNumber, motelPrice);
+            nodes.put(cityNumber, new Node(cityNumber, motelPrice));
         }
 
-        HashMap<Integer[], Integer> gasPrices = new HashMap<Integer[], Integer>();
         while (in.hasNextLine()) {
             int cityOne = in.nextInt();
             int cityTwo = in.nextInt();
             int price = in.nextInt();
 
-            gasPrices.put(new int[]{cityOne, cityTwo}, price);
+            g.addEdge(new Edge(nodes.get(cityOne), nodes.get(cityTwo), price));
         }
+
+        // Compute shortest path
+        int[] distance = g.dijkstra();
+
+        // Print result
+        System.out.println(IntStream.of(distance).sum());
 
     }
 
-    int dijkstra() {
+    static class Graph {
+
+        ArrayList<Edge> edges;
+        PriorityQueue<Node> pq; 
+
+        public Graph() {
+            edges = new ArrayList<Edge>();
+            pq = new PriorityQueue<Node>();
+        }
+
+        public void addEdge(Edge e) {
+             
+        }
+
+        int[] dijkstra() {
+            return new int[]{};
+        }
+    
+    }
+
+    static class Node {
+
+        public int id;
+        public int weight;
+
+        public Node(int id, int weight) {
+            this.id = id;
+            this.weight = weight;
+        }
+    
+    }
+
+    static class Edge {
+
+        public Node n1;
+        public Node n2;
+        public int weight;
+
+        public Edge(Node n1, Node n2, int weight) {
+            this.n1 = n1;
+            this.n2 = n2;
+            this.weight = weight;
+        }
     
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
