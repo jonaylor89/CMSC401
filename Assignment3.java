368768d66b6d41678d89ceaa4dd997f6ad69842b
John Naylor <jonaylor89@gmail.com>
2018-11-15 22:02:33 -0500

    Putting out fires

--- Assignment3.java
+++ Assignment3.java
@@ -6,13 +6,11 @@ import java.util.stream.IntStream;
 
 class Assignment3 {
 
-    // Scanner to get user input
-    static Scanner in = new Scanner(System.in);
-
-    // Temperary data structure to store nodes 
-    static HashMap<Integer, Node> nodes = new HashMap<Integer, Node>();
+    static HashMap<Integer, Integer> motelPrices = new HashMap<Integer, Integer>();
 
     public static void main(String[] argv) {
+
+        Scanner in = new Scanner(System.in);
 
         Graph g = new Graph();
 
@@ -22,13 +20,16 @@ class Assignment3 {
         int numOfHighways = in.nextInt();
         in.nextLine();
 
+
+        motelPrices.put(0, 0);
+
         for (int i = 0; i <= numOfCities; i++) {
             int cityNumber = in.nextInt();
 
             int motelPrice = in.nextInt();
             in.nextLine();
 
-            nodes.put(cityNumber, new Node(cityNumber, motelPrice));
+            motelPrices.put(cityNumber, motelPrice);
         }
 
         while (in.hasNextLine()) {
@@ -36,11 +37,11 @@ class Assignment3 {
             int cityTwo = in.nextInt();
             int price = in.nextInt();
 
-            g.addEdge(new Edge(nodes.get(cityOne), nodes.get(cityTwo), price));
+            g.addEdge(cityOne, motelPrices.get(cityOne), cityTwo, motelPrices.get(cityTwo), price);
         }
 
         // Compute shortest path
-        int[] distance = g.dijkstra();
+        Integer[] distance = g.dijkstra();
 
         // Print result
         System.out.println(IntStream.of(distance).sum());
@@ -49,20 +50,90 @@ class Assignment3 {
 
     static class Graph {
 
-        ArrayList<Edge> edges;
+        int numOfVertices;
+        ArrayList<Edge>[] adjacencyList;
         PriorityQueue<Node> pq; 
 
         public Graph() {
-            distance = new ArrayList<Edge>();
+            numOfVertices = 0;
+            adjacencyList = new ArrayList<Edge>[numOfVertices];
             pq = new PriorityQueue<Node>();
+
+            for (int i = 0; i < numOfVertices; i++) {
+                adjacencyList[i] = new ArrayList<Edge>();
+            }
         }
 
-        public void addEdge(Edge e) {
+        public Graph(int V) {
+            numOfVertices = V;
+            adjacencyList = new ArrayList<Edge>[numOfVertices];
+            pq = new PriorityQueue<Node>();
+
+            for (int i = 0; i < numOfVertices; i++) {
+                adjacencyList[i] = new ArrayList<Edge>();
+            }
+        }
+
+        public void addEdge(int src, int srcWeight, int dest, int destWeight, int weight) {
+
+            Edge e = new Edge(src, srcWeight, dest, destWeight, weight);
+
             edges.add(e);
         }
 
-        int[] dijkstra() {
-            return new int[]{};
+        public Integer[] dijkstra() {
+            int INFINITY = Integer.MAX_VALUE;
+            boolean[] SPT = new boolean[numOfVertices];
+
+            Node[] nodes = new Node[numOfVertices];
+            for (int i = 0; i < numOfVertices; i++) {
+                nodes[i] = new Node(i, motelPrices.get(i), INFINITY);
+            }
+
+            //decrease the distance for the first index
+            nodes[0].distance = 0;
+
+            //add all the vertices to the MinHeap
+            for (int i = 0; i < numOfVertices; i++) {
+                pq.add(nodes[i]);
+            }
+            //while minHeap is not empty
+            while(!pq.isEmpty()){
+                //extract the min
+                Node extractedNode = pq.extractMin();
+
+                //extracted vertex
+                int extractedVertex = extractedNode.id;
+                SPT[extractedVertex] = true;
+
+                //iterate through all the adjacent vertices
+                ArrayList<Edge> list = adjacencyList[extractedVertex];
+                for (int i = 0; i < list.size(); i++) {
+                    Edge edge = list.get(i);
+                    int[] dest = edge.dest;
+                    //only if  destination vertex is not present in SPT
+                    if (SPT[dest[0]] == false) {
+                        ///check if distance needs an update or not
+                        //means check total weight from source to vertex_V is less than
+                        //the current distance value, if yes then update the distance
+                        int newKey = nodes[extractedVertex].distance + edge.weight + dest[1];
+                        int currentKey = nodes[dest[0]].distance;
+                        if(currentKey > newKey){
+                            //get the index which distance's needs a decrease;
+                            int index = pq.indexes[dist[0]];
+
+                            //get the node and update its value
+                            Node node = pq.mH[index];
+                            node.distance = newKey;
+                            pq.bubbleUp(index);
+
+                            nodes[dest[0]].distance = newKey;
+                        }
+                    }
+                }
+            }
+
+            return pq.toArray();
         }
     
     }
@@ -71,23 +142,25 @@ class Assignment3 {
 
         public int id;
         public int weight;
+        public int distance;
 
-        public Node(int id, int weight) {
+        public Node(int id, int weight, int distance) {
             this.id = id;
             this.weight = weight;
+            this.distance = distance;
         }
     
     }
 
     static class Edge {
 
-        public Node n1;
-        public Node n2;
+        public int[] src;
+        public int[] dest;
         public int weight;
 
-        public Edge(Node n1, Node n2, int weight) {
-            this.n1 = n1;
-            this.n2 = n2;
+        public Edge(int src, int srcWeight, int dest, int destWeight, int weight) {
+            this.src = new int[]{src, srcWeight};
+            this.dest = new int[]{dest, destWeight};
             this.weight = weight;
         }
     
